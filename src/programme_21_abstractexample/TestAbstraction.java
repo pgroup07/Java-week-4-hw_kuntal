f3863538dce873e81067ad7a99bd0b7d3bc26bc7
pgroup07 <pgroup07@gmail.com>
2023-06-09 23:54:42 +0100

    week8 wed homework

---
+++ src/programme_21_abstractexample/TestAbstraction.java
@@ -0,0 +1,9 @@
+package programme_21_abstractexample;
+
+public class TestAbstraction {
+    public static void main(String[] args) {
+        //In a real scenario, object is provided through method, e.g., getShape() method
+        Shape s = new Circle();
+        s.draw();
+    }
+}
